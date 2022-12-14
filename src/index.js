7053b7cb9011d23560968483ca050a5f441820cf
Syed Ziaul Habib <roobon@gmail.com>
2022-12-15 00:29:28 +0600

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 //src/index.js
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
